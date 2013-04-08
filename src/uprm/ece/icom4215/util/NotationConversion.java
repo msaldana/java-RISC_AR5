21cda0706f209fae06e0abe38e410118f7c434b4
Manuel <Manuel@192.168.2.5>
2013-04-08 17:04:11 -0400

    Documentation to NotationConversion

--- src/uprm/ece/icom4215/util/NotationConversion.java
+++ src/uprm/ece/icom4215/util/NotationConversion.java
@@ -1,7 +1,20 @@
 package uprm.ece.icom4215.util;
 
+/**
+ * Utility class to facilitate conversions from
+ * hexadecimal to binary, binary to decimal, and decimal to 
+ * binary. Fills more significant bits to 0 in order to declare
+ * 8-bit words in the form of strings.
+ *
+ */
 public class NotationConversion {
-
+	
+	
+	/**
+	 * Returns a binary string representation of the input hex number.
+	 * @param hexString
+	 * @return
+	 */
 	public static String hexToBinary(String hexString){
 		
 		int number = Integer.parseInt(hexString, 16);
@@ -9,10 +22,24 @@ public class NotationConversion {
 			
 	}
 	
+	/**
+	 * Returns an 8-bit word representation of the given hex number. This assumes that
+	 * the number does not consume more than 8-bits. If it does, it simply returns
+	 * the string representation of the binary conversion.
+	 * @param decimal
+	 * @return
+	 */
 	public static String hexTo8BitBinary(int hexInt){
 		return hexTo8BitBinary(hexInt+"");
 	}
 	
+	/**
+	 * Returns an 8-bit word representation of the given hex number. This assumes that
+	 * the number does not consume more than 8-bits. If it does, it simply returns
+	 * the string representation of the binary conversion.
+	 * @param decimal
+	 * @return
+	 */
 	public static String hexTo8BitBinary(String hexString){
 		String binary = hexToBinary(hexString);
 		while (binary.length()<8){
@@ -21,10 +48,24 @@ public class NotationConversion {
 		return binary;
 	}
 	
+	/**
+	 * Returns an 8-bit word representation of the given decimal. This assumes that
+	 * the decimal does not consume more than 8-bits. If it does, it simply returns
+	 * the string representation of the binary conversion.
+	 * @param decimal
+	 * @return
+	 */
 	public static String decimalTo8BitBinary(int decimal){
 		return decimalTo8BitBinary(decimal+"");
 	}
 	
+	/**
+	 * Returns an 8-bit word representation of the given decimal. This assumes that
+	 * the decimal does not consume more than 8-bits. If it does, it simply returns
+	 * the string representation of the binary conversion.
+	 * @param decimal
+	 * @return
+	 */
 	public static String decimalTo8BitBinary(String decimal){
 		String binary = Integer.toBinaryString(Integer.parseInt(decimal));
 		while (binary.length()<8){
@@ -33,15 +74,12 @@ public class NotationConversion {
 		return binary;
 	}
 	
+	/**
+	 * Return a binary string representation of the input decimal.
+	 */
 	public static String binaryToDecimal(String binary){
 		return Integer.parseInt(binary, 2)+"";
 	}
 	
-	public static void main(String[] args){
-		System.out.println(hexToBinary("70"));
-		System.out.println(hexTo8BitBinary("70"));
-		System.out.println(decimalTo8BitBinary("0"));
-		System.out.println(binaryToDecimal("11111111"));
-		
-	}
+
 }
